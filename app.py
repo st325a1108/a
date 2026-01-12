a6466b2362a0494ceefd16e3b69eb3f45ca7c809
st325a1108 <st325a1108a.gotou@urayama.ac.jp>
2026-01-12 19:11:31 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,32 @@
 import streamlit as st
+import random
 
-st.title("はじめてのStreamlitアプリ")
-st.write("Hello, World!")
+st.title("じゃんけんゲーム")
+
+# 横並びレイアウト
+left, right = st.columns(2)
+
+# 左側に画像
+with right:
+    st.image("janken.png", caption="じゃんけん画像")
+    
+
+# 右側にじゃんけんゲーム
+with left:
+    hands = ["グー", "チョキ", "パー"]
+    user = st.radio("手を選んでね", hands)
+    ai = random.choice(hands)
+
+    if st.button("勝負！"):
+        st.write("あなた:", user)
+        st.write("AI:", ai)
+
+        if user == ai:
+            st.info("あいこ")
+        elif (user == "グー" and ai == "チョキ") or \
+             (user == "チョキ" and ai == "パー") or \
+             (user == "パー" and ai == "グー"):
+            st.success("あなたの勝ち！")
+        else:
+            st.error("AIの勝ち…")
+
